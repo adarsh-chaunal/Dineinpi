813048ae5c0ca3b18d9e8bc15a9d118de9883132
adarsh-chaunal <er.adarsh97@gmail.com>
2024-06-23 18:00:29 +0530

    added footer to layout.

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import MainTitle from "@/components/MainTitle";
 
@@ -13,7 +14,7 @@ const Layout = ({children}: Props) => {
             <div className="container mx-auto flex-1 py-10">
                 {children}
             </div>
-            
+            <Footer />
         </div>
     )
 }
